e937cc3f30af9ba872b8d2fc9681805ea394b9b8
prashantmestry <mestry.prashant10@gmail.com>
2025-01-23 20:12:35 +0530

    andt library installed 1

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/vite-deployment/",
-  base: "http://localhost:3001/",
+  base: "/app/",
   dev: {
     host: true,
     port: 8080,
